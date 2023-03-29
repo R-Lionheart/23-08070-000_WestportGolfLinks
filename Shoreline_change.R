0a454d7717bc4b95a5138ac5eb21300c86216782
Regina Lionheart <rlionheart@herrerainc.com>
2023-03-28 17:20:09 -0700

    Added inflection point, visual only.

--- Shoreline_change.R
+++ Shoreline_change.R
@@ -4,25 +4,33 @@
 
 library(tidyverse)
 
-## Arbitrary base point information
-## All info is in meters!
+## Arbitrary baseline was drawn approximately 350m from shoreline
+## Each historical shoreline from 2006 to present was measured to the baseline.
+## All info is in meters.
 
-South <- data.frame(year = c("7-2006", "9-2009", "9-2011", "8-2016", "3-2020", "8-2022"),
+
+## Southern benchmark at approximately 46°53'13.57"N, 124° 7'34.81"W
+south <- data.frame(year = c("7-2006", "9-2009", "9-2011", "8-2016", "3-2020", "8-2022"),
                  distance_m = c(305.00, 308.49, 313.71, 338.23, 344.00, 348.42), 
-                 position = "South"
-)
+                 position = "south")
 
-North <- data.frame(year = c("7-2006", "9-2009", "9-2011", "8-2016", "3-2020", "8-2022"),
+## Northern benchmark at approximately 46°54'6.84"N, 124° 7'54.29"W
+north <- data.frame(year = c("7-2006", "9-2009", "9-2011", "8-2016", "3-2020", "8-2022"),
                     distance_m = c(322.55, 317.65, 311.69, 309.64, 316.66, 319.76), 
-                    position = "North"
-)
+                    position = "north")
 
-Dist <- South %>% 
-  rbind(North)
-Dist$year <- factor(Dist$year , levels=c("6-1990", "7-2006", "9-2009", "9-2011", "8-2016", "3-2020", "8-2022"))
+## Inflection point (where active erosion appears to be mitigated by Corps nourishment),
+## approximately 46°53'49.91"N, 124° 7'48.71"W.
+inflection <- data.frame(year = c("7-2006", "9-2009", "9-2011", "8-2016", "3-2020", "8-2022"),
+                         distance_m = c(328.97, 332.19, 333.04, 331.54, 335.62, 337.26), 
+                         position = "inflection")
+
+distance_to_baseline <- south_location %>% 
+  rbind(north_location)
+distance_to_baseline$year <- factor(distance_to_baseline$year , levels=c("6-1990", "7-2006", "9-2009", "9-2011", "8-2016", "3-2020", "8-2022"))
 
 
-ggplot(Dist, aes(group=position, y=distance_m, x=year, color=position)) +
+ggplot(distance_to_baseline, aes(group=position, y=distance_m, x=year, color=position)) +
   geom_line()
 
 t <- Dist %>% 
