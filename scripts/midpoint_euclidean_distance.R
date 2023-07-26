10afe372c06be08425d195868d1659775344205a
Regina Lionheart <rlionheart@herrerainc.com>
2023-07-26 13:19:58 -0700

    Change meters to feet through midpoint calcs.

--- scripts/midpoint_euclidean_distance.R
+++ scripts/midpoint_euclidean_distance.R
@@ -1,13 +1,11 @@
 ## RLionheart
-## 21-0771-001
-## January 2023
 ## Shoreline Conservation Areas, Washington State Parks
 
 profile.pattern <- "prof_16|prof_17"
 source("scripts/src/assign_profile_parks.R")
 
 
-## Euclidean distances
+## Euclidean distances: in feet
 euclidean <- complete.profile %>%
   group_by(profile, year) %>%
   mutate(x_midpoint = ((min(x) + max(x))/2)) %>%
@@ -16,9 +14,10 @@ euclidean <- complete.profile %>%
   unique() %>%
   arrange(profile, year) %>%
   group_by(profile) %>%
-  mutate(euc_dist_to_BP = sqrt(((BasePoint_X - x_midpoint)^2) + ((BasePoint_Y -  y_midpoint)^2))) %>%
+  mutate(feet_to_BP = (sqrt(((BasePoint_X - x_midpoint)^2) +
+                                  ((BasePoint_Y -  y_midpoint)^2))) *  3.28084) %>% ## Convert m to feet
   drop_na() %>%
-  mutate(net_profile_slope = ifelse(euc_dist_to_BP[which.min(year)] < euc_dist_to_BP[which.max(year)],
+  mutate(net_profile_slope = ifelse(feet_to_BP[which.min(year)] < feet_to_BP[which.max(year)],
                                 "Accretion", "Erosion")) %>%
   mutate(profile = factor(profile, levels = c("1", "2", "3", "4", "5", "6", "7",
                                               "8", "9", "10", "48", "11", "12",
@@ -30,15 +29,14 @@ euclidean <- complete.profile %>%
                                               "51", "52", "41", "53", "54", "42",
                                               "43", "44", "45", "46", "47")))
 
-
 ### Extract equation parameters
 equation.details <- euclidean %>%
-  select(profile, Park, year, euc_dist_to_BP) %>%
+  select(profile, Park, year, feet_to_BP) %>%
   unique() %>%
   drop_na() %>%
   group_by(profile) %>%
   mutate(dummy_year = row_number()) %>%
-  do(model = lm(euc_dist_to_BP ~ dummy_year, data = .)) %>%
+  do(model = lm(feet_to_BP ~ dummy_year, data = .)) %>%
   mutate(intercept = coef(model)[1],
          slope = coef(model)[2],
          rsq = summary(model)$r.squared,
@@ -56,6 +54,7 @@ equation.details <- euclidean %>%
                                               "51", "52", "41", "53", "54", "42",
                                               "43", "44", "45", "46", "47")))
 
+## All data combined to one df
 toplot <- euclidean %>%
   left_join(equation.details, by = "profile") %>%
   mutate(shoreline_profile = ifelse(pvalue < 0.05 & slope > 0, "Significant Accretion",
@@ -75,17 +74,16 @@ toplot <- euclidean %>%
 
 ## Visualize euclidean distance from average Euclidean distance of each year
 midpoint.euc.dist.plot <- ggplot(toplot %>% drop_na(), 
-                                 aes(year, euc_dist_to_BP, 
+                                 aes(year, feet_to_BP, 
                                      fill = shoreline_profile, group = shoreline_profile)) +
   scale_fill_manual(values=c("grey55", "grey54", "#04A1FF", "tomato2", "grey")) +
-  #facet_grid(rows = vars(profile)) +
   facet_wrap(~profile) +
   geom_col(position = position_dodge(width = 1)) +
   geom_line(aes(group = shoreline_profile), position = position_dodge(width = 1),
             linewidth = 1, color = "black") +
   geom_smooth(method = "lm", se = TRUE, color="black") +
   xlab("Year") +
-  ylab("Distance in m from BasePoint") + 
+  ylab("Distance in feet from BasePoint") + 
   theme(axis.text.x = element_blank(),
         axis.text.y = element_blank()) +
   guides(fill=guide_legend(title="")) +
@@ -98,7 +96,7 @@ pearson.correlation <- toplot %>%
   group_by(profile) %>%
   mutate(dummy_year = row_number()) %>%
   group_by(profile) %>%
-  summarize(pearson_correlation=cor(dummy_year, euc_dist_to_BP)) %>%
+  summarize(pearson_correlation=cor(dummy_year, feet_to_BP)) %>%
   mutate(relationship = ifelse(pearson_correlation > 0 & pearson_correlation < 1, 
                                "Accretion", "Erosion")) %>%
   mutate(profile = factor(profile, levels = c("1", "2", "3", "4", "5", "6", "7",
@@ -128,7 +126,7 @@ write.csv(results.table, "data_secondary/profiles_with_equations.csv", row.names
 
 ## Download csv for clustering
 cluster <- euclidean %>%
-  select(profile:year, euc_dist_to_BP) %>%
+  select(profile:year, feet_to_BP) %>%
   unique()
 write.csv(cluster, "data_secondary/profiles_with_midpoint_distance.csv", 
           row.names = FALSE)
